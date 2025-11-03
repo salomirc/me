323d550624ff2d210c8d4d4a9a3b5f93c5af4c38
Ciprian Salomir <ciprian.salomir@gmail.com>
2025-11-03 02:35:49 +0200

    SEND_MAIL_BASE_URL bugfix

--- site/src/jsMain/kotlin/org/example/me/helpers/BaseUrlHelper.kt
+++ site/src/jsMain/kotlin/org/example/me/helpers/BaseUrlHelper.kt
@@ -1,7 +1,7 @@
 package org.example.me.helpers
 
 private const val BASE_URL: String = "https://jsonplaceholder.typicode.com"
-private const val SEND_MAIL_BASE_URL: String = "https://cosulbio.linkpc.net/"
+private const val SEND_MAIL_BASE_URL: String = "https://cosulbio.linkpc.net"
 
 fun String.toEndpointUrl() = "$BASE_URL$this"
 fun String.toSendMailEndpointUrl() = "$SEND_MAIL_BASE_URL$this"
